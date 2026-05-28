2e010b63cb5c436e4325d230c1cc274e93dffd09
elimai <marchuk.artemii2009@yandex.ru>
2026-05-28 19:36:35 +0300

    Добавил валидацию входных данных

--- java/Validator.java
+++ java/Validator.java
@@ -1,2 +1,15 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Validator {
+
+    public static boolean isFileExists(String filePath) {
+
+        return Files.exists(Path.of(filePath));
+    }
+
+    public static boolean isValidKey(int key, int alphabetSize) {
+
+        return key >= 0 && key < alphabetSize;
+    }
 }
