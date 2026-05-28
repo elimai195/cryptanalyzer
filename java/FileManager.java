0441932e2695ab6c296e2dffad093a6e9af6d8ef
elimai <marchuk.artemii2009@yandex.ru>
2026-05-28 19:09:08 +0300

    добавил режимы шифрования и расшифровки

--- java/FileManager.java
+++ java/FileManager.java
@@ -1,2 +1,39 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileManager {
+
+    public static String readFile(String filePath) {
+
+        try {
+
+            return Files.readString(
+                    Path.of(filePath),
+                    StandardCharsets.UTF_8
+            );
+
+        } catch (IOException e) {
+
+            System.out.println("Ошибка чтения файла.");
+            return "";
+        }
+    }
+
+    public static void writeFile(String filePath, String text) {
+
+        try {
+
+            Files.writeString(
+                    Path.of(filePath),
+                    text,
+                    StandardCharsets.UTF_8
+            );
+
+        } catch (IOException e) {
+
+            System.out.println("Ошибка записи файла.");
+        }
+    }
 }
